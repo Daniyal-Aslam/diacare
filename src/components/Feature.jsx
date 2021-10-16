36f68e3852f17860e4c05648e94ca37cecd9e299
Daniyal Aslam <80858661+Daniyal-Aslam@users.noreply.github.com>
2021-10-16 06:32:43 -0700

    Update Feature.jsx

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Headcomp from './Headcomp';
 import Header from './Header';
+import Footer from './Footer';
 
  const Feature = () => {
     return (
         <> 
+     <Header/>
             <section className="feature_banner">
                 <Headcomp
                 head="Feature page"
                 tag="Feature / Practice Area’s"
                 />
             </section>
+     <Footer/>
         </>
     )
 }
