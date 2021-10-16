6a180ff696136c876ebea54649e5a5e30b22ec36
Daniyal Aslam <80858661+Daniyal-Aslam@users.noreply.github.com>
2021-10-16 06:34:12 -0700

    Update Pricing.jsx

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Header from './Header'; 
+import Footer from './Footer'; 
 import Headcomp from './Headcomp';
 
  const Pricing = () => {
     return (
         <>
+     <Header/>
           <section className="pricing_banner">
               <Headcomp
               head="Pricing page"
               tag="Pricing / Practice Area’s"
             />
         </section>  
+     <Footer/>
         </>
     )
 }
