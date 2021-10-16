e91c8446b4ba6262a7e51a65c480debfa5642074
Daniyal Aslam <80858661+Daniyal-Aslam@users.noreply.github.com>
2021-10-16 06:33:12 -0700

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PracticeCard from './PracticeCard';
 import Headcomp from './Headcomp';
 import Header from './Header';
-import Header from './Footer';
+import Footer from './Footer';
 import Datacard from './Datacard';
 import CountrySelect from './Countryapi';
  const Home = () => {
