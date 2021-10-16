ba65a1fc8b8ecf46ddc35fe7f2989683babfe17a
Daniyal Aslam <80858661+Daniyal-Aslam@users.noreply.github.com>
2021-10-16 06:29:23 -0700

    change-route

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@
 import {Switch, Route } from 'react-router'; 
 import Home from './components/Home'; 
 import Feature from './components/Feature'; 
-import Header from './components/Header'; 
-import Footer from './components/Footer'; 
 import Pricing from './components/Pricing'; 
 import Contact from './components/Contact'; 
 import './style/app.scss';   
 
 function App() {
   return (
-    <>  
-    <Header/>
+    <>   
     <Switch>
       <Route exact path="/" component={Home}/> 
       <Route  path="/Feature" component={Feature}/>
       <Route path="/Pricing" component={Pricing}/>
       <Route path="/Contact" component={Contact}/>
-    </Switch>
-    <Footer/>
+    </Switch> 
     </>
   );
 }
